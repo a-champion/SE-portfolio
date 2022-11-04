8ac01629d6f33f2ef22899b17cc908ed03431d21
a-champion <a.champion1506@gmail.com>
2022-11-04 09:02:00 -0600

    connected and styled nav bar

---
+++ app.js
@@ -0,0 +1,18 @@
+
+
+
+
+$(() => {
+    // let titles = ['software developer', 'gamer', 'funny guy', 'creative type', 'generic description here type guy.'];
+    // const changeText = (titlesArr) =>{ //takes an array and will change the rotatingTitle class to the next title. Resets at the end of the
+    //     for(let i = 0; i < titlesArr.length; i++){
+    //         ('.rotatingTitle').text(`${titlesArr[i]}`).delay(800);
+    //     };
+    // };
+    
+    // $('.rotatingTitle').fadeOut();
+
+    // $('body').setInterval(changeText(titles), 3000);
+
+
+});
